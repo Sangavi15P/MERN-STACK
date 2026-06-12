c314314da34fa771e34ced9904fa3eee29cf6f2a
Sangavi15P <sansangavis@gmail.com>
2026-06-12 10:21:31 +0530

    added variales.js

---
+++ variables.js
@@ -0,0 +1,13 @@
+//variables declaration using var
+var name = "Sangavi"
+console.log(name)
+
+//variables declaration using let
+{
+let age = 18
+console.log(18)
+}
+
+//variables declaration using const
+const college = "VETIAS"
+console.log(college)
