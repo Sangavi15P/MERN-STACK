dfe66be4296fb7d8f80c7ca91f264f1b5c9a4745
Sangavi15P <sansangavis@gmail.com>
2026-06-12 15:22:10 +0530

    added function.js

---
+++ function.js
@@ -0,0 +1,13 @@
+//NON-PARAMETRIC FUNCTIONS
+function greet() {
+    console.log("Hello World");
+}
+greet();
+
+
+//PARAMETRIC FUNCTIONS
+function add(a,b) {
+    console.log(a+b);
+    return a + b;
+}
+add(5,3);
