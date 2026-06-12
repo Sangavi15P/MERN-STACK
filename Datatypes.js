d1e65f809878d16e63e5cc42affba09c53142e0f
Sangavi15P <sansangavis@gmail.com>
2026-06-12 15:12:27 +0530

    added Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,56 @@
+//PRIMITIVE DATATYPE
+ 
+//NUMBERS
+console.log("Numbers");
+var age = 25;
+console.log(age);
+
+//strings
+console.log("strings");
+var name = "Rohitha";
+console.log(name);
+
+//BOOLEAN
+console.log("Boolean");
+var isstudent = "true";
+console.log(isstudent);
+
+//BIG INT
+console.log("big int");
+var big = 12345678901234567890638583001588256775709361601490;
+console.log(big);
+
+//NULL
+console.log("null");
+var emptyvalue = null;
+console.log(emptyvalue);
+
+//UNDEFINED
+console.log("undefined");
+var undefinedvalue;
+console.log(undefinedvalue);
+
+//SYMBOL
+console.log("symbol");
+var symbolsvalue = ('symbol')
+console.log(symbolsvalue);
+
+
+
+
+
+//NON-PRIMITIVE DATA TYPES
+
+//OBJECTS
+console.log("object");
+var person = {
+    name: "Ramya",
+    age: 19
+};
+console.log(person.age);
+
+//ARRAY
+console.log("array");
+var fruits = ["Apple", "Orange", "Grapes", "Fig"];
+console.log(fruits.length);
+console.log(fruits[3]);
