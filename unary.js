5bc3ebe7e7a2ef6869ef095f3db30538ff0ba843
Sangavi15P <sansangavis@gmail.com>
2026-06-12 13:55:15 +0530

    modified unary.js

--- unary.js
+++ unary.js
@@ -3,3 +3,10 @@ console.log(c++);
 console.log(c--);
 console.log(++c);
 console.log(--c);
+
+
+//ternary operator
+var a = 20;
+var b = 10;
+var result = (a > b) ? "a is greater" : "b is greater";
+console.log(result);
