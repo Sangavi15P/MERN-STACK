17c6d9493f44b2f134e26593d146595060b05771
Sangavi15P <sansangavis@gmail.com>
2026-06-12 15:51:20 +0530

    added conditional.js

---
+++ conditional.js
@@ -0,0 +1,15 @@
+var light = "green"
+
+if (light == "green") {
+    console.log("Go!");
+}
+else if (light == "Yellow") {
+    console.log("Wait");
+}
+else if (light == "Red") {
+    console.log("stop");
+}
+else {
+    console.log("Invalid light color");
+}
+console.log("outside If");
---
+++ fav function.js
@@ -0,0 +1,10 @@
+var actor = "VIJAY";
+var player = "DHONI";
+var movie = "NANBAN";
+ 
+function favourite(){
+    console.log("favourite actor" + actor);
+    console.log("favourite player" + player);
+    console.log("favourite movie" + movie);
+}
+favourite()
